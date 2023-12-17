ad9a2becb8fcdb44db079e4fd20f8b4d9fe8863e
061alirezaa <113263245+061alirezaa@users.noreply.github.com>
2023-12-18 02:29:14 +0330

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -203,27 +203,47 @@ check_status() {
 ## Banner
 banner() {
 	cat <<- EOF
-		${ORANGE}
-		${ORANGE} ______      _     _     _               
-		${ORANGE}|___  /     | |   (_)   | |              
-		${ORANGE}   / / _ __ | |__  _ ___| |__   ___ _ __ 
-		${ORANGE}  / / | '_ \| '_ \| / __| '_ \ / _ \ '__|
-		${ORANGE} / /__| |_) | | | | \__ \ | | |  __/ |   
-		${ORANGE}/_____| .__/|_| |_|_|___/_| |_|\___|_|   
+  
+  ${ORANGE}    .----.                              .----------.       .----------. 
+${ORANGE}    .   _   \  ..-'''-.                  /          /       /          /  
+${ORANGE}   /  .' )   | \.-'''\ \                /   ______.'       /   ______.'   
+${ORANGE}  |   (_.    /        | |              /   /_             /   /_          
+${ORANGE}   \     ,  /      __/ /              /      '''--.      /      '''--.    
+${ORANGE}    `'-'/  /      |_  '.             '___          `.   '___          `.  
+${ORANGE}.-.    /  /          `.  \               `'.         |      `'.         | 
+${ORANGE}\  '--'  /             \ '.                 )        |         )        | 
+${ORANGE} '-....-'               , |  ,.--.  ......-'        /  ......-'        /  
+  ${ORANGE}                      | | //    \ \          _..'`   \          _..'`   
+ ${ORANGE}                      / ,' \\    /  '------'''         '------'''        
+${ORANGE}               -....--'  /   `'--'                                        
+${ORANGE}              `.. __..-'                                                 
+${ORANGE}
 		${ORANGE}      | |                                
 		${ORANGE}      |_|                ${RED}Version : ${__version__}
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by 93.55 (ALIREZA)${WHITE}
 	EOF
 }
 
 ## Small Banner
 banner_small() {
 	cat <<- EOF
-		${BLUE}
-		${BLUE}  ░▀▀█░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀▄
-		${BLUE}  ░▄▀░░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
-		${BLUE}  ░▀▀▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} ${__version__}
+
+
+ ${BLUE}    _       ___  __   ___ __   _  
+ ${BLUE}   /_)  )    )   )_)  )_   /  /_) 
+${BLUE}   / /  (__ _(_  / \  (__  /_ / /  
+                                
+
+
+
+ ${BLUE}    _       ___  __   ___ __   _  
+ ${BLUE}   /_)  )    )   )_)  )_   /  /_) 
+${BLUE}   / /  (__ _(_  / \  (__  /_ / /  
+                                
+
+ 
+		${WHITE} ${__version__}
 	EOF
 }
 
